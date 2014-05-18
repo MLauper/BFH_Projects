e3af0eb112faec319f36a48d09814cde583fb374
Alain <alain@diemeute.ch>
2014-05-18 13:17:39 +0200

    Lottery backend system

---
+++ src/ch/bfh/ti/lottery/backend/Lottery.java
@@ -0,0 +1,13 @@
+package ch.bfh.ti.lottery.backend;
+
+/**
+ * This is the logic of the lottery system.
+ */
+public class Lottery {
+
+    /**
+     * Constructor
+     */
+    public Lottery() {
+    }
+}
