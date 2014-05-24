b2f1e586c1cd9bdea2dd93c11a8929d0c4e1dcb3
Reto Zoss <reto@zoss.li>
2014-05-24 22:23:14 +0200

    Updated gitignore, created UITester and UI fxml

---
+++ src/ch/bfh/ti/lottery/ui/UITester.java
@@ -0,0 +1,16 @@
+package ch.bfh.ti.lottery.ui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class UITester extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
