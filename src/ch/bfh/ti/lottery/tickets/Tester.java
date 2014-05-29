abed7337524e18d495732f147f0fa119f5859797
Alain <alain@diemeute.ch>
2014-05-29 18:33:32 +0200

    marshalling

--- src/ch/bfh/ti/lottery/tickets/Tester.java
+++ src/ch/bfh/ti/lottery/tickets/Tester.java
@@ -1,6 +1,14 @@
 package ch.bfh.ti.lottery.tickets;
 
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
 /**
  * Created by alain on 24.05.14.
  */
@@ -35,6 +43,32 @@ public class Tester {
         System.out.println(
                 TicketTools.setValidity(tickets.getTicket().get(0), 8)
         );
+
+
+        JAXBContext jc = null;
+        try {
+            jc = JAXBContext.newInstance();
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+        Marshaller m = null;
+        try {
+            m = jc.createMarshaller();
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+
+        OutputStream os = null;
+        try {
+            os = new FileOutputStream( "test_tickets.xml" );
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            m.marshal( tickets, os );
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
 
         System.out.println("end");
 
