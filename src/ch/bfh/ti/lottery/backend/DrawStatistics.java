27b7cd456ecb79e03afbafd2d7bab42dc88d47e1
Alain <alain@diemeute.ch>
2014-06-01 15:25:39 +0200

    lottery system with draw statistics

---
+++ src/ch/bfh/ti/lottery/backend/DrawStatistics.java
@@ -0,0 +1,28 @@
+package ch.bfh.ti.lottery.backend;
+
+/**
+ * Created by alain on 01.06.14.
+ */
+public class DrawStatistics {
+
+    int tot5NumOkAnd2StarOk = 0;
+    int tot5NumOkAnd1StarOk = 0;
+    int tot5NumOkAnd0StarOk = 0;
+    int tot4NumOkAnd2StarOk = 0;
+    int tot4NumOkAnd1StarOk = 0;
+    int tot4NumOkAnd0StarOk = 0;
+    int tot3NumOkAnd2StarOk = 0;
+    int tot2NumOkAnd2StarOk = 0;
+    int tot3NumOkAnd1StarOk = 0;
+    int tot3NumOkAnd0StarOk = 0;
+    int tot1NumOkAnd2StarOk = 0;
+    int tot2NumOkAnd1StarOk = 0;
+    int tot2NumOkAnd0StarOk = 0;
+
+    public DrawStatistics() {
+    }
+
+
+
+
+}
--- src/ch/bfh/ti/lottery/backend/Lottery.java
+++ src/ch/bfh/ti/lottery/backend/Lottery.java
@@ -1,12 +1,26 @@
 package ch.bfh.ti.lottery.backend;
 
+import ch.bfh.ti.lottery.tickets.TicketTools;
+import ch.bfh.ti.lottery.tickets.Tickets;
+
 /**
  * This is the logic of the lottery system.
  */
 public class Lottery {
 
+    Tickets lotteryTicketPool = null;
     private int nextTicketId = 0;
 
+    public Lottery(Tickets lotteryTicketPool) {
+        this.lotteryTicketPool = new Tickets();
+    }
+
+    public void generateTickets(int toGenerateTickets, int playsPerTicket){
+        for (int i = 0; i < toGenerateTickets; i++) {
+            lotteryTicketPool.getTicket().add(TicketTools.generateRandomTicket(this.nextTicketId, playsPerTicket));
+            this.nextTicketId++;
+        }
+    }
 
 
 
