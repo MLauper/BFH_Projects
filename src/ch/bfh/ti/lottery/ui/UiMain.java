30be76d8f6a0a25fe689b3add52cc2ea758eb19b
Reto Zoss <reto@zoss.li>
2014-06-07 11:32:40 +0200

    div Änderungen
    Lang Files und UI Controller generiert.

--- src/ch/bfh/ti/lottery/ui/UiMain.java
+++ src/ch/bfh/ti/lottery/ui/UiMain.java
@@ -1,13 +1,26 @@
 package ch.bfh.ti.lottery.ui;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class UiMain extends Application {
-
+	private AnchorPane rootLayout;
+    private Scene scene;
 	@Override
 	public void start(Stage primaryStage) {
-		
+		try {
+            FXMLLoader loader = new FXMLLoader(
+                    UiMain.class.getResource("UILottery.fxml"));
+            rootLayout = (AnchorPane) loader.load();
+            scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 	}
 
 	public static void main(String[] args) {
