aae66365502e955e80b6c770f3d577ea50f8dec9
Reto Zoss <reto@zoss.li>
2014-06-09 20:29:47 +0200

    Conflict geflickt...

--- src/ch/bfh/ti/lottery/StartLottery.java
+++ src/ch/bfh/ti/lottery/StartLottery.java
@@ -11,29 +11,20 @@ public class StartLottery {
 
         Lottery lottery = new Lottery();
 
-<<<<<<< HEAD
-        lottery.generateTickets(100, 3);
-        //lottery.writeTicketPoolToXml("/Users/Reto/Github/BFH/src/ch/bfh/ti/lottery/ticPool.xml");
-        lottery.addXmlTickets("src/ch/bfh/ti/lottery/ticPool.xml", true);
-=======
-        lottery.generateTickets(1000, 3);
-        lottery.writeTicketPoolToXml("ch/bfh/ti/lottery/ticPool.xml");
-        lottery.addXmlTickets("ch/bfh/ti/lottery/ticPool.xml", true);
->>>>>>> FETCH_HEAD
 
-        lottery.setSetLuckyNumbers(28,16,7,30,13);
+        lottery.generateTickets(1000, 3);
+        //lottery.writeTicketPoolToXml("ch/bfh/ti/lottery/ticPool.xml");
+        //lottery.addXmlTickets("ch/bfh/ti/lottery/ticPool.xml", true);
+
+       lottery.setSetLuckyNumbers(28,16,7,30,13);
        lottery.setSetLuckyStars(10,9);
        lottery.setSetLuckySuperStars("L994R","V252G","R835F","B923C");
-//
-<<<<<<< HEAD
-       lottery.draw();
-       
-       lottery.printStats();
-        //lottery.writeTicketPoolToXml("/Users/alain/Documents/GitHub/BFH/projects/src/ch/bfh/ti/lottery/ticPool.xml");
-=======
+
+
         lottery.draw();
-        lottery.writeTicketPoolToXml("ch/bfh/ti/lottery/ticPool.xml");
->>>>>>> FETCH_HEAD
+        lottery.printStats();
+        //lottery.writeTicketPoolToXml("ch/bfh/ti/lottery/ticPool.xml");
+
 
         System.out.println("Totel tickets in pool: " + lottery.getTotalTicketsInPool());
 
--- src/ch/bfh/ti/lottery/ui/UiController.java
+++ src/ch/bfh/ti/lottery/ui/UiController.java
@@ -523,6 +523,7 @@ public class UiController {
 			i++;
 			
 		}
+		
 	}
 
 	
